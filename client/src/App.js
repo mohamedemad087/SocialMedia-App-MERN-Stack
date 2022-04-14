af827c6e81a2dba750500419574fb936f7380179
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-14 15:46:23 +0200

    add pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
-      <BrowserRouter>
-        <Container maxWidth="lg">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/auth" component={Auth} />
-          </Switch>
-        </Container>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
+          <Route
+            path="/auth"
+            exact
+            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+          />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,36 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
 import { useDispatch } from "react-redux"; // this allows to dispatch an action
 
 import { getPosts } from "../../actions/postsActions";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
+import Pagination from "../Pagination.jsx";
 import useStyles from "./styles";
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
+
   const classes = useStyles();
+
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-  // currentId is for when clear() is work, will change currentId. so, posts will change immediately
-
   return (
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
+    <Grow in>
+      <Container>
+        <Grid
+          className={classes.gridContainer}
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={6} md={9}>
+            <Posts setCurrentId={setCurrentId} />
           </Grid>
-        </Container>
-      </Grow>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
   );
 };
 
